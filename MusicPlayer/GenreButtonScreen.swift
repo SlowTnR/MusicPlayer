aeb9136cf5dfa893cd46c6366807655389effb50
Ilja Patrushev <slowtnr@gmail.com>
2020-10-15 15:36:29 +0300

    Add UI.

---
+++ MusicPlayer/GenreButtonScreen.swift
@@ -0,0 +1,29 @@
+//
+//  GenreButtonScreen.swift
+//  MusicPlayer
+//
+//  Created by Ilja Patrushev on 15.10.2020.
+//
+
+import UIKit
+
+class GenreButtonScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
